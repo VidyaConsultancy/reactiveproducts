93c5b9f9461754a8a7955e127cccf970911b3412
Arpit <arpit_jain@live.com>
2022-08-04 14:33:57 +0530

    redux: protecting routes

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,49 +1,77 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { logoutSuccess } from "../../store/auth.slice";
+import { useRouterNavigate } from "../../hooks/useRouterNavigate";
 
-export const Header = () => (
-  <header>
-    <nav className="navbar navbar-expand-lg bg-light">
-      <div className="container-fluid">
-        <NavLink className="navbar-brand" to="/">
-          Navbar
-        </NavLink>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <NavLink className="nav-link" aria-current="page" to="/">
-                Home
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/products">
-                Products
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/signin">
-                Login
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/signup">
-                Register
-              </NavLink>
-            </li>
-          </ul>
+export const Header = () => {
+  const dispatch = useDispatch();
+  const navigate = useRouterNavigate();
+  const authState = useSelector((state) => state.auth);
+  const { accessToken } = authState;
+
+  const logout = () => {
+    dispatch(logoutSuccess());
+    localStorage.clear();
+    navigate('/signin');
+  };
+
+  return (
+    <header>
+      <nav className="navbar navbar-expand-lg bg-light">
+        <div className="container-fluid">
+          <NavLink className="navbar-brand" to="/">
+            Navbar
+          </NavLink>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <NavLink className="nav-link" aria-current="page" to="/">
+                  Home
+                </NavLink>
+              </li>
+              {accessToken ? (
+                <React.Fragment>
+                  <li className="nav-item">
+                    <NavLink className="nav-link" to="/products">
+                      Products
+                    </NavLink>
+                  </li>
+                  <li className="nav-item">
+                    <button className="nav-link" onClick={logout}>
+                      Logout
+                    </button>
+                  </li>
+                </React.Fragment>
+              ) : (
+                <>
+                  <li className="nav-item">
+                    <NavLink className="nav-link" to="/signin">
+                      Login
+                    </NavLink>
+                  </li>
+                  <li className="nav-item">
+                    <NavLink className="nav-link" to="/signup">
+                      Register
+                    </NavLink>
+                  </li>
+                </>
+              )}
+            </ul>
+          </div>
         </div>
-      </div>
-    </nav>
-  </header>
-);
+      </nav>
+    </header>
+  );
+};
--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+import { useSelector } from "react-redux";
 import { ProductList } from "../product-list/ProductList";
 import { ProductAdd } from "../product-add/ProductAdd";
 import { ProductEdit } from "../product-edit/ProductEdit";
@@ -10,25 +11,39 @@ import { Header } from "../header/Header";
 // header, footer, main, aside, video, audio, section, article, nav
 
 export const Main = () => {
+  const authState = useSelector((state) => state.auth);
+  const { accessToken } = authState;
   // const mainClassNames = ["main", "main-content"];
   // if(state === 'sme') {
   //   mainClassNames.push('sm');
   // }
   const mainStyle = {
-    backgroundColor: '#f8f8f8',
-  }
+    backgroundColor: "#f8f8f8",
+  };
+
+  const PageNotFound = (
+    <div className="404-page">
+      <h3>404 <small>Page not found</small></h3>
+    </div>
+  )
+
   return (
     <div className="content">
-      <Header className="" />
+      <Header />
       {/* <main className={mainClassNames.join(" ")}> */}
       <main className="main" style={mainStyle}>
         <Routes>
           <Route path="" exact element={<h1>Home page</h1>} />
           <Route path="/signup" exact element={<Signup />} />
           <Route path="/signin" exact element={<Signin />} />
-          <Route path="/products" element={<ProductList />} />
-          <Route path="/products/add" element={<ProductAdd />} />
-          <Route path="/products/edit/:id" element={<ProductEdit />} />
+          {accessToken ? (
+            <>
+              <Route path="/products" element={<ProductList />} />
+              <Route path="/products/add" element={<ProductAdd />} />
+              <Route path="/products/edit/:id" element={<ProductEdit />} />
+            </>
+          ) : null}
+          <Route path="*" exact element={PageNotFound} />
         </Routes>
       </main>
       <footer>&copy; All rights reserved</footer>
